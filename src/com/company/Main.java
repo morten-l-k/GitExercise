e523a571218af5c4ddcc9517c403412b916b1b7e
morten-l-k <mort75q3@stud.kea.dk>
2022-02-25 13:17:34 +0100

    added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
+public class Main {
+Scanner keyboard = new Scanner(System.in);
+
+        public void greet(){
+          System.out.println("Indtast dit navn: ");
+          String sc = keyboard.next();
+          System.out.println("Hej " + sc);
+        }
     public static void main(String[] args) {
-        System.out.println("It Works!");
+        Main obj = new Main();
+
+          System.out.println("It Works!");
+          obj.greet();
+
     }
 }
